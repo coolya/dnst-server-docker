761828f5a14044db7d7ac7177607dc6f8858f284
Kolja Dummann <kolja.dummann@logv.ws>
2015-08-08 15:16:46 +0200

    alalalal

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 ls -al /dnst-conf
 mkdir -p /root/.klei/DoNotStarveTogether
 ln -s /dnst-conf /root/.klei/DoNotStarveTogether
-ls -al /root/.klei/DoNotStarveTogether
+cd  /root/.klei/DoNotStarveTogether
+pwd
+ls -al
 cd /dnst/bin
 ./dontstarve_dedicated_server_nullrenderer  -console
