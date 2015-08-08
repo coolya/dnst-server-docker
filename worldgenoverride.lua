15697ae51f939db81c228b386b90e54ba856dece
Kolja Dummann <kolja.dummann@logv.ws>
2015-08-08 16:12:50 +0200

    world config

---
+++ worldgenoverride.lua
@@ -0,0 +1,77 @@
+return {
+ override_enabled = true,
+       unprepared = { -- "never", "rare", "default", "often", "always"
+               berrybush = "often",
+               cactus = "default",
+               carrot = "often",
+               mushroom = "default",
+       },
+       misc = {
+               autumn = "default", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               boons = "often", -- "never", "rare", "default", "often", "always"
+               branching = "default", -- "never", "least", "default", "most"
+               day = "longday", -- "default", "longday", "longdusk", "longnight", "noday", "nodusk", "nonight", "onlyday", "onlydusk", "onlynight"
+               frograin = "never", -- "never", "rare", "default", "often", "always"
+               lightning = "never", -- "never", "rare", "default", "often", "always"
+               loop = "default", -- "never", "default", "always"
+               season_start = "spring", -- "default", "winter", "spring", "summer", "random"
+               spring = "longseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               summer = "shortseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               touchstone = "often", -- "never", "rare", "default", "often", "always"
+               weather = "rare", -- "never", "rare", "default", "often", "always"
+               wildfires = "never", -- "never", "rare", "default", "often", "always"
+               winter = "shortseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               world_size = "large", -- "default", "medium", "large", "huge"
+       },
+       animals = { -- "never", "rare", "default", "often", "always"
+               alternatehunt = "default",
+               angrybees = "never",
+               beefalo = "often",
+               beefaloheat = "default",
+               bees = "default",
+               birds = "default",
+               butterfly = "default",
+               buzzard = "default",
+               catcoon = "default",
+               frogs = "rare",
+               hunt = "often",
+               lightninggoat = "default",
+               moles = "default",
+               penguins = "often",
+               perd = "default",
+               pigs = "default",
+               rabbits = "often",
+               tallbirds = "default",
+       },
+       monsters = { -- "never", "rare", "default", "often", "always"
+               bearger = "never", --
+               chess = "default", --
+               deciduousmonster = "rare", --
+               deerclops = "never", --
+               dragonfly = "never", --
+               goosemoose = "never", --
+               houndmound = "default", --
+               hounds = "rare", --
+               krampus = "never", --
+               liefs = "rare", --
+               lureplants = "rare", --
+               merm = "default", --
+               spiders = "default", --
+               tentacles = "default", --
+               walrus = "default", --
+       },
+       resources = { -- "never", "rare", "default", "often", "always"
+               flint = "often",
+               flowers = "default",
+               grass = "often",
+               marshbush = "default",
+               meteorshowers = "rare",
+               meteorspawner = "default",
+               reeds = "often",
+               rock = "often",
+               rock_ice = "often",
+               sapling = "often",
+               trees = "often",
+               tumbleweed = "default",
+       },
+}
