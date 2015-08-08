1cadd786dda9887ddd5617820bffdb8b1555ef4b
Kolja Dummann <kolja.dummann@logv.ws>
2015-08-08 15:25:40 +0200

    fuck it

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 ls -al /dnst-conf
 mkdir -p /root/.klei/DoNotStarveTogether
-ln -s /dnst-conf/ /root/.klei/DoNotStarveTogether
-cd  /root/.klei/DoNotStarveTogether
+ln -s /dnst-conf /root/.klei/DoNotStarveTogether
+cd  /root/.klei
 pwd
-ls -al
+ls 
+cd  DoNotStarveTogether
+pwd
+ls 
 cd /dnst/bin
 ./dontstarve_dedicated_server_nullrenderer  -console
