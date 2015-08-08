e2ccbbd01145ee2a6267d4ec2d401c52f89c50c5
Kolja Dummann <kolja.dummann@logv.ws>
2015-08-08 15:10:47 +0200

    sdkjsdfp

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 ls -al /dnst-conf
-
-mkdir -p /root/.klei/DoNotStarveTogether
-ln -sf /dnst-conf /root/.klei/DoNotStarveTogether
+ln -s /dnst-conf /root/.klei/DoNotStarveTogether
 ls -al /root/.klei/DoNotStarveTogether
 cd /dnst/bin
 ./dontstarve_dedicated_server_nullrenderer  -console
