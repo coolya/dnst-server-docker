8e667d12c9ef61e2aacbbcb700eb1836b3b4bfd6
Kolja Dummann <kolja.dummann@logv.ws>
2015-08-16 17:28:24 +0200

    update before starting the server

--- start.sh
+++ start.sh
@@ -2,11 +2,6 @@
 ls -al /dnst-conf
 mkdir -p /root/.klei
 ln -s /dnst-conf /root/.klei/DoNotStarveTogether
-cd  /root/.klei
-pwd
-ls 
-cd  DoNotStarveTogether
-pwd
-ls 
+/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /dnst +app_update 343050 validate +quit 
 cd /dnst/bin
 ./dontstarve_dedicated_server_nullrenderer  -console
