f65b1b9b7fdb5365358378a705bdbaa7d32468dc
Kolja Dummann <kolja.dummann@logv.ws>
2015-09-23 21:44:28 +0200

    new world and stuff

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,15 +1,28 @@
 return {
-    ["workshop-347079953"] = { enabled = true },
-    ["workshop-370373189"] = { enabled = true },
-    ["workshop-366676924"] = { enabled = true },
-    ["workshop-384396097"] = { enabled = true },
-    ["workshop-382177939"] = { enabled = true },
-    ["workshop-375850593"] = { enabled = true },
-    ["workshop-373991022"] = { enabled = true },
-    ["workshop-375859599"] = { enabled = true },
-    ["workshop-396026892"] = { enabled = true },
-    ["workshop-356420397"] = { enabled = true },
-    ["workshop-357875628"] = { enabled = true },
-    ["workshop-362175979"] = { enabled = true },
-    ["workshop-434859111"] = { enabled = true }
+["workshop-503187227"] = { enabled = true }, --world gen
+["workshop-488009136"] = { enabled = true }, --archery
+["workshop-447911453"] = { enabled = true }, -- hollow stump
+["workshop-347079953"] = { enabled = true }, -- food values
+["workshop-370373189"] = { enabled = true }, -- adv farming
+["workshop-366676924"] = { enabled = true }, -- tiki torch
+["workshop-356435289"] = { enabled = true }, --fish farm
+["workshop-385006082"] = { enabled = true }, -- path light
+["workshop-384396097"] = { enabled = true }, --spider cage
+["workshop-382177939"] = { enabled = true }, --storm cellar
+["workshop-375850593"] = { enabled = true }, -- extra equip
+["workshop-458940297"] = { enabled = true }, -- food values toolkit
+["workshop-373991022"] = { enabled = true }, --global player icons
+["workshop-375859599"] = { enabled = true }, --health info
+["workshop-375859599"] = { enabled = true }, --infinite tent
+["workshop-434859111"] = { enabled = true }, --infinite siesta
+["workshop-354533909"] = { enabled = true }, --koalefants
+["workshop-396026892"] = { enabled = true }, --large chest
+["workshop-515834211"] = { enabled = true }, --large signed chest
+["workshop-516523980"] = { enabled = true }, --mining machine
+["workshop-356420397"] = { enabled = true }, -- no respawn panal
+["workshop-395433621"] = { enabled = true }, --rabbit nest
+["workshop-403291762"] = { enabled = true }, --raining cats an dogs
+["workshop-396822875"] = { enabled = true }, -- spike trap
+["workshop-469801526"] = { enabled = true }, -- Ancient items
+["workshop-362175979"] = { enabled = true } -- wormhole
 }
---
+++ worloverrides_new.lua
@@ -0,0 +1,79 @@
+ return {
+ override_enabled = true,
+       unprepared = { -- "never", "rare", "default", "often", "always"
+               berrybush = "often",
+               cactus = "default",
+               carrot = "often",
+               mushroom = "default",
+       },
+       misc = {
+               task_set = "default", -- Biomes: "classic", "cave_default"
+               start_location = "default", -- Start: "plus", "darkness", "caves"
+               autumn = "longseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               boons = "often", -- "never", "rare", "default", "often", "always"
+               branching = "default", -- "never", "least", "default", "most"
+               day = "longday", -- "default", "longday", "longdusk", "longnight", "noday", "nodusk", "nonight", "onlyday", "onlydusk", "onlynight"
+               frograin = "never", -- "never", "rare", "default", "often", "always"
+               lightning = "never", -- "never", "rare", "default", "often", "always"
+               loop = "default", -- "never", "default", "always"
+               season_start = "spring", -- "default", "winter", "spring", "summer", "random"
+               spring = "default", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               summer = "shortseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               touchstone = "often", -- "never", "rare", "default", "often", "always"
+               weather = "rare", -- "never", "rare", "default", "often", "always"
+               wildfires = "never", -- "never", "rare", "default", "often", "always"
+               winter = "default", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+               world_size = "large", -- "default", "medium", "large", "huge"
+       },
+       animals = { -- "never", "rare", "default", "often", "always"
+               alternatehunt = "always",
+               angrybees = "rare",
+               beefalo = "often",
+               beefaloheat = "default",
+               bees = "default",
+               birds = "default",
+               butterfly = "default",
+               buzzard = "default",
+               catcoon = "default",
+               frogs = "rare",
+               hunt = "often",
+               lightninggoat = "default",
+               moles = "default",
+               penguins = "always",
+               perd = "default",
+               pigs = "default",
+               rabbits = "often",
+               tallbirds = "default",
+       },
+       monsters = { -- "never", "rare", "default", "often", "always"
+               bearger = "never",
+               chess = "default",
+               deciduousmonster = "default",
+               deerclops = "never",
+               dragonfly = "default",
+               goosemoose = "never",
+               houndmound = "default",
+               hounds = "rare",
+               krampus = "rare",
+               liefs = "rare",
+               lureplants = "rare",
+               merm = "default",
+               spiders = "default",
+               tentacles = "default",
+               walrus = "default",
+       },
+       resources = { -- "never", "rare", "default", "often", "always"
+               flint = "often",
+               flowers = "default",
+               grass = "often",
+               marshbush = "rare",
+               meteorshowers = "rare",
+               meteorspawner = "default",
+               reeds = "default",
+               rock = "often",
+               rock_ice = "often",
+               sapling = "often",
+               trees = "often",
+               tumbleweed = "often",
+       }
+}
