0ec22b554e4376a0b9a8183a913099226b46fa44
Kolja Dummann <kolja.dummann@logv.ws>
2015-09-23 21:50:19 +0200

    mods

--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -1,13 +1,26 @@
-ServerModSetup("347079953")
-ServerModSetup("370373189")
-ServerModSetup("366676924")
-ServerModSetup("384396097")
-ServerModSetup("382177939")
-ServerModSetup("375850593")
-ServerModSetup("373991022")
-ServerModSetup("375859599")
-ServerModSetup("396026892")
-ServerModSetup("356420397")
-ServerModSetup("357875628")
-ServerModSetup("362175979")
-ServerModSetup("434859111")
+ServerModSetup("503187227") --world gen
+ServerModSetup("488009136") --archery
+ServerModSetup("447911453") -- hollow stump
+ServerModSetup("347079953") -- food values
+ServerModSetup("370373189") -- adv farming
+ServerModSetup("366676924") -- tiki torch
+ServerModSetup("356435289") --fish farm
+ServerModSetup("385006082") -- path light
+ServerModSetup("384396097") --spider cage
+ServerModSetup("382177939") --storm cellar
+ServerModSetup("375850593") -- extra equip
+ServerModSetup("458940297") -- food values toolkit
+ServerModSetup("373991022") --global player icons
+ServerModSetup("375859599") --health info
+ServerModSetup("375859599") --infinite tent
+ServerModSetup("434859111") --infinite siesta
+ServerModSetup("354533909") --koalefants
+ServerModSetup("396026892") --large chest
+ServerModSetup("515834211") --large signed chest
+ServerModSetup("516523980") --mining machine
+ServerModSetup("356420397") -- no respawn panal
+ServerModSetup("395433621") --rabbit nest
+ServerModSetup("403291762") --raining cats an dogs
+ServerModSetup("396822875") -- spike trap
+ServerModSetup("469801526") -- Ancient items
+ServerModSetup("362175979")-- wormhole
