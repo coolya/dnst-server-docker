66bdfbc4adbea9b0e7f5ae6a32d746ff2a3e3654
Kolja Dummann <kolja.dummann@logv.ws>
2015-09-23 22:39:47 +0200

    save mods

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 ls -al /dnst-conf
 mkdir -p /root/.klei
 ln -s /dnst-conf /root/.klei/DoNotStarveTogether
+cp /dnst/mods/dedicated_server_mods_setup.lua /dnst/mods/dedicated_server_mods_setup.lua.bak
 /steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /dnst +app_update 343050 validate +quit 
+mv /dnst/mods/dedicated_server_mods_setup.lua.bak /dnst/mods/dedicated_server_mods_setup.lua
 cd /dnst/bin
 ./dontstarve_dedicated_server_nullrenderer  -console
